15365c7624e6810fd4024691c859e916984ee22e
zenny <136650902+skilogistics@users.noreply.github.com>
2023-10-20 12:08:25 -0700

    Create free_stack.c

---
+++ free_stack.c
@@ -0,0 +1,18 @@
+free_stack.c
+#include "monty.h"
+/**
+* free_stack - frees a doubly linked list
+* @head: head of the stack
+*/
+void free_stack(stack_t *head)
+{
+	stack_t *aux;
+
+	aux = head;
+	while (head)
+	{
+		aux = head->next;
+		free(head);
+		head = aux;
+	}
+}
